b837a0804fb738802f93113c3aaf03e3bd498273
Thomas Tursics <thomas@tursics.de>
2021-12-10 00:29:02 +0100

    add some error pages + use /upload path only if login is enabled

---
+++ src/components/403-Forbidden.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="row mt-1 mb-3">
+      <div class="col-10 offset-1">
+        <p>{{ $t('message.error.http403') }}</p>
+        <button class="btn btn-sm btn-secondary" @click="$router.go(-1)">{{ $t('message.error.goBack') }}</button>
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Forbidden',
+};
+</script>
+
+<style scoped>
+
+</style>
