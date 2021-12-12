cc61e681557ed1fd3b76623d80fc2eaf0b1eef07
Thomas Tursics <thomas@tursics.de>
2021-12-12 02:32:54 +0100

    style top navigation

--- src/components/Top.vue
+++ src/components/Top.vue
@@ -3,20 +3,27 @@
     Native HTML-Tag names are not allowed for Vue components. **/
 
 <template>
-  <header>
-    <nav class="navbar-container navbar-dark bg-primary mb-3 border-bottom">
-      <div class="content-wrapper navbar navbar-expand-sm">
-        <app-link class="navbar-brand d-inline-block" :to="{ path: '/'}">
-          <div v-for="(image, imageIndex) in images" class="project-title-container">
-            <img v-if="image.src" :src="image.src" class="app-logo" :title="image.description" :key="imageIndex">
+  <header class="mb-3 border-bottom">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container-fluid">
+
+        <app-link class="navbar-brand" :to="{ path: '/'}">
+          <div v-for="(image, imageIndex) in images"
+            class="project-title-container"
+            v-bind:key="imageIndex"
+          >
+            <img v-if="image.src" :src="image.src"
+              class="d-inline-block align-text-top app-logo"
+              height="24"
+              :title="image.description"
+              :key="imageIndex"
+            >
             <span v-if="image.text">{{image.text}}</span>
           </div>
         </app-link>
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarSupportedContent"
+
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
           aria-controls="navbarSupportedContent"
           aria-expanded="false"
           aria-label="Toggle navigation"
@@ -24,24 +31,25 @@
           <span class="navbar-toggler-icon"></span>
         </button>
 
-        <div id="navbarSupportedContent" class="collapse navbar-collapse">
-          <ul class="navbar-nav mr-auto">
-            <li v-for="link in links" class="nav-item">
-              <app-link v-if="!link.web" class="nav-link" :to="{ path: link.href }">
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li v-for="(link, linkIndex) in links"
+              class="nav-item"
+              v-bind:key="linkIndex"
+            >
+              <app-link class="nav-link" :to="link.web ? link.href : { path: link.href }">
                 {{ link.title }}
               </app-link>
-              <app-link v-if="link.web" class="nav-link" :to="link.href">
-                {{ link.title }}
-              </app-link>
-            </li>
-            <li v-if="$env.keycloak.enableLogin" class="nav-item small">
-              <a v-if="!authenticated" v-on:click="login()" class="nav-link p-0 login">{{ $t('message.header.subnav.login') }}</a>
-              <a v-if="authenticated" v-on:click="logout()" class="nav-link p-0 login">{{ $t('message.header.subnav.logout') }}</a>
             </li>
           </ul>
+
+          <span v-if="$env.keycloak.enableLogin" class="navbar-text">
+            <a v-if="!authenticated" v-on:click="login()" class="btn btn-outline-dark login">{{ $t('message.header.subnav.login') }}</a>
+            <a v-if="authenticated" v-on:click="logout()" class="btn btn-outline-dark login">{{ $t('message.header.subnav.logout') }}</a>
+          </span>
         </div>
+
       </div>
-      
     </nav>
   </header>
 </template>
@@ -102,47 +110,4 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-  @import '../styles/bootstrap_theme';
-
-  .navbar-container {
-    .navbar {
-      .project-title-container {
-        margin-right: 2rem;
-        position: inline-block;
-        vertical-align: top;
-        text-align: right;
-
-        .app-logo {
-            width: 120px;
-          }
-          .project-name {
-          text-transform: uppercase;
-          font-size: 0.8em;
-          font-weight: bold;
-          margin-top: -10px;
-        }
-      }
-
-      .navbar-nav {
-        font-size: 1.1rem;
-        font-weight: bold;
-        .nav-link.active {
-          color: $white;
-        }
-      }
-
-    .login{
-       color: white;
-        cursor: pointer;
-        &:hover {
-          text-decoration: underline;
-        }
-    }
-    }
-  }
-  
-
-  .brand-name {
-    line-height: 48px;
-  }
 </style>
