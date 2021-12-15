b001ab7c5207b0ba6cdb55f69cedcb4de1bfd833
Thomas Tursics <thomas@tursics.de>
2021-12-15 11:01:20 +0100

    remove styling from data info box

--- src/components/DataInfoBox.vue
+++ src/components/DataInfoBox.vue
@@ -1,26 +1,26 @@
 <template>
-  <div class="data-info-box card mt-3">
+  <div class="data-info-box card mt-3 border border-light">
     <a data-toggle="collapse"
        :href="`#dist-${collapseId}`"
        v-if="collapse">
-      <div class="card-header">
+      <div class="card-header bg-transparent border-0">
         <h6 class="card-title text-dark">{{ title || 'no title given' }}</h6>
       </div>
     </a>
     <app-link class="text-dark text-decoration-none" :to="{ path: linkTo, query: Object.assign({}, { locale: $route.query.locale }) }">
-      <div class="card-header pb-0"
+      <div class="card-header bg-transparent border-0 pb-0"
            v-if="!collapse">
         <div class="card-title">
-          <span class="data-info-box-title mb-0 text-dark">{{ title || 'no title given' }}</span>
+          <span class="data-info-box-title mb-0 fw-bold fs-5 text-dark">{{ title || 'no title given' }}</span>
           <ul class="subtitle-list list-inline subtitle align-middle mr-1" v-if="!isEmpty(source)">
             <!-- Distributor -->
-            <li class="list-inline-item small">
+            <li class="list-inline-item small m-0 me-2">
               <img class="mr-1 border border-dark" width="24" alt="Catalogue Flag" v-if="has(source, 'sourceImage')" :src="source.sourceImage">
               <span v-if="has(source, 'sourceTitle') && !isNil(source.sourceTitle)">{{ getTranslationFor(source.sourceTitle, $i18n.locale, []) }}</span>
             </li>
             
             <!-- Dates -->
-            <li class="list-inline-item small"
+            <li class="list-inline-item small m-0 me-2"
                 v-for="(md, i) in metadata"
                 :key="i"
                 v-if="has(md, 'title') && !isNil(md.title) && has(md, 'value')">
@@ -34,7 +34,7 @@
 
         <!-- Formats and licenses -->
         <div class="" v-if="!isNil(metadata) && isObject(metadata)">
-          <span class="formats badge badge-secondary rounded-pill mr-1"
+          <span class="formats badge bg-secondary rounded-pill me-1"
                 v-for="(tag, i) in bodyTags"
                 v-if="showDistributionFormat(tag)"
                 :key="i"
@@ -223,106 +223,12 @@
 <style lang="scss" scoped>
   @import '../styles/bootstrap_theme';
 
-  // Truncated description text
-  .truncated-gradient {
-    height: 100%;
-    max-height: 10rem;
-    overflow: hidden;
-    position: relative;
-    &:after {
-      content: "";
-      position: absolute;
-      bottom: 0;
-      left: 0;
-      background: linear-gradient(to bottom, rgba(255, 255, 255, 0), rgba(255, 255, 255, 1));
-      height: 4rem;
-      width: 100%;
-    }
-  }
-
   .data-info-box {
-    border-left: 3px solid rgba($primary, .8);
-    // box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
-    transition: all .3s cubic-bezier(.25, .8, .25, 1);
+    border-color: #ced4da !important;
+
     &:hover {
-      border-left: 3px solid rgba($primary, 1.0);
-      box-shadow: 0 8px 16px rgba(0, 0, 0, 0.25), 0 4px 8px rgba(0, 0, 0, 0.22);
+      border-color: $dark !important;
       cursor: pointer;
-    }
-
-    .data-info-box-title {
-      font-weight: 600;
-      font-size: 1.1rem;
-    }
-
-    .card-header {
-      border: none;
-      background-color: inherit;
-      ul.subtitle-list {
-        li {
-          margin: 0;
-          &:last-child:after {
-            content: ""
-          }
-          &:after {
-            content: " ";
-            white-space: normal;
-            word-spacing: 1em; /* = nav@padding-right - this actually makes width */
-            margin: 0 5px;
-          }
-        }
-      }
-      .badge-pill {
-        border-radius: 10px;
-      }
-    }
-
-    .formats {
-      &[type="HTML"] {
-        background-color: #55a1ce;
-      }
-      &[type="JSON"] {
-        background-color: #ef7100;
-      }
-      &[type="XML"] {
-        background-color: #ef7100;
-      }
-      &[type="TXT"] {
-        background-color: #74cbec;
-      }
-      &[type="CSV"] {
-        background-color: #dfb100;
-      }
-      &[type="XLS"] {
-        background-color: #2db55d;
-      }
-      &[type="ZIP"] {
-        background-color: #686868;
-      }
-      &[type="API"] {
-        background-color: #ec96be;
-      }
-      &[type="PDF"] {
-        background-color: #e0051e;
-      }
-      &[type="RDF"],
-      &[type="NQUAD"],
-      &[type="NTRIPLES"],
-      &[type="TURTLE"] {
-        background-color: #0b4498;
-      }
-    }
-
-    .card-footer {
-      display:flex;
-      flex-direction: row;
-      justify-content: space-between;
-
-      i {
-        font-size: 32px;
-        vertical-align: middle;
-        margin: 0 0 .1em 0;
-      }
     }
   }
 </style>
